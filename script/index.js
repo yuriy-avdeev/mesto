26d23cde13553a399cc97a22af2a7026aa412905
Yury Avdeev <avdeiev@gmail.com>
2021-01-04 18:19:23 +0300

    after fixing

--- script/index.js
+++ script/index.js
@@ -1,4 +1,4 @@
-let openPopup = document.querySelector('.profile__editor-popup');
+let buttonOpenPopup = document.querySelector('.profile__editor-popup');
 let popup = document.querySelector('.popup');
 let closePopup = popup.querySelector('.popup__close');
 let buttotSendForm = popup.querySelector('.popup__submit');
@@ -8,27 +8,23 @@ let jobInput = formElement.querySelector('.popup__input_type_activity');
 let userName = document.querySelector('.profile__name');
 let userActivity = document.querySelector('.profile__activity');
 
-function popupStart () {
-    nameInput.value = userName.textContent;
-    jobInput.value = userActivity.textContent;
-
-    popupToggle();
-}
-
 function popupToggle() {
     popup.classList.toggle('popup_active');
 }
 
-openPopup.addEventListener('click', popupStart);
-closePopup.addEventListener('click', popupToggle);
-
-function handleFormSubmit (evt) {
-    evt.preventDefault(); 
-
-    userName.textContent = nameInput.value;
-    userActivity.textContent = jobInput.value;
-
+function openPopup () {
+    nameInput.value = userName.textContent;
+    jobInput.value = userActivity.textContent;
     popupToggle();
 }
 
+function handleFormSubmit (evt) {
+    evt.preventDefault(); 
+    userName.textContent = nameInput.value;
+    userActivity.textContent = jobInput.value;
+    popupToggle();
+}
+
+buttonOpenPopup.addEventListener('click', openPopup);
+closePopup.addEventListener('click', popupToggle);
 formElement.addEventListener('submit', handleFormSubmit);
