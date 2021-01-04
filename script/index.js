2b1f3ef73e6c5276d5d7787d62069402b17484a8
Yury Avdeev <avdeiev@gmail.com>
2021-01-04 08:46:02 +0300

    after fixed

--- script/index.js
+++ script/index.js
@@ -1,33 +1,34 @@
 let openPopup = document.querySelector('.profile__editor-popup');
-let overlay = document.querySelector('.overlay');
-let closePopup = overlay.querySelector('.popup__close');
-let buttotSendForm = overlay.querySelector('.popup__submit');
+let popup = document.querySelector('.popup');
+let closePopup = popup.querySelector('.popup__close');
+let buttotSendForm = popup.querySelector('.popup__submit');
 let formElement = document.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__form_type_name');
-let jobInput = formElement.querySelector('.popup__form_type_activity');
+let nameInput = formElement.querySelector('.popup__input_type_name');
+let jobInput = formElement.querySelector('.popup__input_type_activity');
 let userName = document.querySelector('.profile__name');
 let userActivity = document.querySelector('.profile__activity');
 
-nameInput.getAttribute('value'); 
-jobInput.getAttribute('value');
+function popupStart () {
+    nameInput.value = userName.textContent;
+    jobInput.value = userActivity.textContent;
 
-function popupToggle() {
-    overlay.classList.toggle('overlay_active');
-    if (nameInput.value === " " && jobInput.value === " ") {
-        nameInput.value = userName.textContent;
-        jobInput.value = userActivity.textContent;
-    }
+    popupToggle();
 }
 
-openPopup.addEventListener('click', popupToggle);
+function popupToggle() {
+    popup.classList.toggle('popup_active');
+}
+
+openPopup.addEventListener('click', popupStart);
 closePopup.addEventListener('click', popupToggle);
-buttotSendForm.addEventListener('click', popupToggle);
 
 function handleFormSubmit (evt) {
     evt.preventDefault(); 
 
     userName.textContent = nameInput.value;
     userActivity.textContent = jobInput.value;
+
+    popupToggle();
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
